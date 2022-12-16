c040b3f2ab3974fcd954266f504a732aec0d3bc2
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-12-16 12:03:13 -0400

    feat: primeiro desafio finalizado

---
+++ src/main/java/PrimeiroDesafio.java
@@ -0,0 +1,58 @@
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+public class PrimeiroDesafio {
+
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            imprime("Olá, irei organizar os números que você digitar da seguinte forma:");
+            imprime(" - Primeiro os números pares em ordem crescente;");
+            imprime(" - Após irei organizar os números ímpares em ordem decrescente.");
+            imprime("Aviso, eles tem que ser acima de '0'");
+            imprime("Quantos números você quer organizar? ");
+            int quantidadeNumeros = verificaPositivo();
+            List<Integer> numerosPares = new ArrayList<Integer>();
+            List<Integer> numerosImpares = new ArrayList<Integer>();
+            for (int posicao = 0; posicao < quantidadeNumeros; posicao += 1) {
+                imprime("Digite um número: ");
+                int numero = verificaPositivo();
+                if (numero % 2 == 0) {
+                    numerosPares.add(numero);
+                } else {
+                    numerosImpares.add(numero);
+                }
+            }
+            Collections.sort(numerosPares);
+            numerosImpares.sort(Collections.reverseOrder());
+            imprimeLista(numerosPares);
+            imprimeLista(numerosImpares);
+            scanner.close();
+        } catch (Exception exception) {
+            imprime("Ops! Alguma coisa deu errado. Rode o programa novamente.");
+        }
+    }
+
+    public static void imprimeLista(@NotNull List<Integer> lista) {
+        for (Integer integer : lista) {
+            imprime(integer);
+        }
+    }
+
+    public static int verificaPositivo() {
+        Scanner scanner = new Scanner(System.in);
+        int valor;
+        do {
+            valor = scanner.nextInt();
+            if (valor < 1) {
+                imprime("Por favor, digite um valor maior que '0'.");
+            }
+        } while (valor < 1);
+        return valor;
+    }
+
+    public static void imprime(Object mensagem) {
+        System.out.println(mensagem);
+    }
+}
